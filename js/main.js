82bfaf90cf41b259d8e4468e3ec33e886a96e31f
talithaizza <talitha.luthfia@mail.ugm.ac.id>
2019-03-12 09:57:21 +0700

    pwa1

--- js/main.js
+++ js/main.js
@@ -1,104 +1,102 @@
-$(document).ready(function(){
-    // inisialisasi API url
-    var url = 'https://my-json-server.typicode.com/talithaizza/belajar-api/mahasiswa';
+$(document).ready(function () {
+    // inisiasi API url
+    var _url = 'https://my-json-server.typicode.com/talithaizza/belajar-api/mahasiswa';
+    // var _url = 'http://localhost/b2/index.php';
 
-    // untuk menampung semua data mahasiswa
-    var result='';
-    //untuk menampung semua gender mahasiswa
-    var gender = [];
+    // untuk menampung data semua mahasiswa
+    var result = '';
+
     // untuk menampung gender sbg option di select
-    var gender_result= '';
-    
+    var gender_result = '';
+    // untuk menampung gender semua mahasiswa
+    var gender = [];
 
-    // $.get(url, function (data) {
-        function renderPage(data){
-            $.each(data, function(key, items){
+
+    // $.get(_url,function (data) {
+    function renderPage(data) {
+        $.each(data, function (key, items) {
             _gend = items.gender;
 
             result += '<div>' +
-                '<h3>'+items.name+'</h3>' +
-                '<p>'+_gend+'</p>' +
+                '<h3>' + items.name + '</h3>' +
+                '<p>' + _gend + '</p>' +
                 '</div>';
 
-            if($.inArray(_gend, gender) === -1){
+            if ($.inArray(_gend, gender) === -1) {
                 gender.push(_gend);
-                gender_result += "<option value='"+_gend+"'>"+_gend+"</option>";
+                gender_result += "<option value='" + _gend + "'>" + _gend + "</option>";
             }
         });
 
         $('#mhs-list').html(result);
-        $('#mhs-select').html("<option value='semua'>semua</option>"+gender_result);
-        }
-        
-    // });
+        $('#mhs-select').html("<option value='semua'>semua</option>" + gender_result);
+        // });
+    }
 
     var networkDataReceive = false;
-    /* 
-    * Start balapan antara service dengan cache 
-    * Fresh data from online service
+    /*
+    * start balapan antara service dengan cache
+    * fresh data from online service
     * */
-    var networkUpdate = fetch(url).then(function(response){
+    var networkUpdate = fetch(_url).then(function (response) {
         return response.json();
-    }).then(function (data){
+    }).then(function (data) {
         networkDataReceive = true;
         renderPage(data);
     });
 
-    /* Ambilkan data dalam local cache */
-    caches.match(url).then(function(response){
-        if(!response) throw Error( "no data on cache" )
-            return response.json();
-    }).then(function(data){
-        if(!networkDataReceive){
+    /* ambilkan data dalam local cache */
+    caches.match(_url).then(function (response) {
+        if (!response) throw Error("no data on cache")
+        return response.json();
+    }).then(function (data) {
+        if (!networkDataReceive){
             renderPage(data);
             console.log("render from cache");
         }
-    }).cache(function(){
+    }).catch(function () {
         return networkUpdate;
     });
 
+    // filter data
 
-
-    //filter data
-    $("#mhs-select").on('change', function(){
+    $("#mhs-select").on('change', function () {
         updateListMahasiswa($(this).val());
     });
 
-    function updateListMahasiswa(opt){
+    function updateListMahasiswa(opt) {
         var result = '';
-        var url2 = url;
+        var _url2 = _url;
 
-        if (opt !==  'semua'){
-            url2 = url + '?gender=' + opt;
+        // cek opsi yang dipilih
+        if (opt !== 'semua'){
+            _url2 = _url + '?gender='+opt;
         }
 
-        $.get(url2, function (data) {
-            $.each(data, function(key, items){
+        $.get(_url2,function (data) {
+            $.each(data, function (key, items) {
                 _gend = items.gender;
-    
+
                 result += '<div>' +
                     '<h3>'+items.name+'</h3>' +
                     '<p>'+_gend+'</p>' +
                     '</div>';
             });
-    
+
             $('#mhs-list').html(result);
-           
-        
         });
     }
-
 });
 
-if('serviceWorker' in navigator){
-    window.addEventListener('load', function(){
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', function () {
         navigator.serviceWorker.register('/serviceworker.js').then(
-            function(reg){
-                //regristrasi service worker berhail
-                console.log('SW registration success, scope : ', reg.scope);
-            }, function(err){
-                //reg failed
-                console.log('SW regristration failed : ', err);
+            function (reg) {
+                // registerasi service worker berhasil
+                console.log('SW registration success, scope :',reg.scope);
+            }, function (err) {
+                // reg failed
+                console.log('SW registration faild : ', err);
             }
         )
     })
